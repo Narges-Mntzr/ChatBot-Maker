9922f1aae79244869932cf1de6fc38e967a7442a
Narges-Mntzr <narges.montazeri81@gmail.com>
2023-11-17 11:19:31 +0330

    Add: add views & urls of register,login & logout.

--- TODO.md
+++ TODO.md
@@ -3,17 +3,14 @@ This file is updated during the implementation of the project and deleted at the
 
 ### General:
 - [ ] Add favIcon.
-- [ ] Deploying the project using Hamravesh and creating a CI/CD pipeline with three build/test/deploy stages.
-- [ ] Hash password.
+- [x] Deploying the project using Hamravesh and creating a CI/CD pipeline with three build/test/deploy stages.
+- [x] Hash password.
 - [ ] Connect database to postgress.
-- [x] Example
+- [ ] Set logout
+- [ ] Landing page.
 
 ### Questions:
-1.  فازاول: ساخت ادمین جنگو برای مشاهده و تغییر داده‌ها توسط کاربر ادمین. (۱ ساعت) 
-توی این مرحله دقیقا به جز اضافه کردن داده‌ها به ادمین باید چی کار کنیم؟
-
-2. منظور از محتوای چت‌بات دقیقا چیه؟ به هرکدوم از اون پاراگراف‌ها می‌گیم؟
 
 3. مدل "نظر" برای چیست؟
 
-4. prompt چیه دقیقا؟
+
--- myproject/chatbot/views.py
+++ myproject/chatbot/views.py
@@ -1,3 +1,56 @@
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.password_validation import validate_password
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+# @login_required(login_url='chatbot:home')
+def home(request):
+    if request.user.is_authenticated:
+        return render(request,'chatbot/chat-list.html')
+    else:
+        return render(request,'chatbot/landing.html')
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return HttpResponseRedirect(reverse("chatbot:home"))
+        else:
+            return render (request,'chatbot/login.html', {'error_message':'Username or password is incorrect!'})
+    else:
+        return render(request,'chatbot/login.html')
+
+def logout(request):
+    #todo: GET or POST
+    if request.method == 'GET':
+        auth.logout(request)
+    return HttpResponseRedirect(reverse("chatbot:login"))
+
+def register(request):
+    if request.method == "POST":
+        if request.POST['password'] == request.POST['password-confirm']:
+            try:
+                validate_password(request.POST['password'])
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password'])
+                if(request.POST['group']!='normal'):
+                    group = Group.objects.get(name=request.POST['group'])
+                    user.groups.add(group)
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse("chatbot:home"))
+            except IntegrityError as e:
+                return render (request,'chatbot/register.html', {'error_message':'email is already taken!'})
+            except Exception as e:
+                 return render(request,'chatbot/register.html',{'error_message':e.error_list[0]})
+        else:
+            return render (request,'chatbot/register.html', {'error_message':'Password does not match!'})
+    else:
+        return render(request,'chatbot/register.html')
+
+
+    
