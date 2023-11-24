7958ccf76624cda3db14a33fca6722b62c93dd8c
Narges-Mntzr <narges.montazeri81@gmail.com>
2023-11-25 01:24:07 +0330

    Update: update last_mssage_date with signal.

--- myproject/chatbot/apps.py
+++ myproject/chatbot/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ChatbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chatbot'
+
+    def ready(self):
+        import chatbot.signals  # Replace 'your_app' with the actual name of your app
+
---
+++ myproject/chatbot/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Message 
+
+@receiver(post_save, sender=Message)
+def message_post_save(sender, instance, created, **kwargs):
+    if created:
+        instance.chat.last_message_date = max(instance.chat.last_message_date,instance.pub_date)
+        instance.chat.save()
